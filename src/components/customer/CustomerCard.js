540c09d9b1ae96139c017d72df1fd319f5e5d723
Andrew Birt <abirt1986@gmail.com>
2021-12-03 17:10:26 -0500

    list all customers

--- src/components/customer/CustomerCard.js
+++ src/components/customer/CustomerCard.js
@@ -4,7 +4,5 @@ import "./Customer.css";
 export const CustomerCard = ({ customer }) => (
   <section className="customer">
     <h3 className="customer__name">{customer.name}</h3>
-    <div className="customer__breed">{customer.breed}</div>
-    <address className="location__address">{customer.location.name}</address>
   </section>
 );
